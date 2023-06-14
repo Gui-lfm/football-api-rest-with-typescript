e01239739a9d7f92fbfabb0a88001e9367efb56b
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-06-14 17:05:37 -0300

    tdd login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,44 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+import SequelizeUsers from '../database/models/SequelizeUsers';
+import loginMock from './mocks/login.mock';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Testes do fluxo de Users e Login', function () {
+  it('Se o login foi feito com sucesso, deve retornar um token com status 200', async function () {
+    const parameters = loginMock.validBody;
+    const userMock = SequelizeUsers.build(loginMock.existingUser);
+    sinon.stub(SequelizeUsers, 'findOne').resolves(userMock);
+
+    const response = await chai.request(app).post('/login').send(parameters);
+
+    expect(response.status).to.be.equal(200)
+    expect(response.body.token).not.to.be.undefined
+  });
+
+  it('Se o login não tiver o campo "email", deve retornar um erro com status 400', async function () {
+    const parameters = loginMock.bodyWithoutEmail;
+
+    const { body, status } = await chai.request(app).post('/login').send(parameters);
+
+    expect(status).to.be.equal(400)
+    expect(body.message).to.equal('All fields must be filled');
+  })
+
+  it('Se o login não tiver o campo "password", deve retornar um erro com status 400', async function () {
+    const parameters = loginMock.bodyWithInvalidPassword;
+
+    const { body, status } = await chai.request(app).post('/login').send(parameters);
+
+    expect(status).to.be.equal(400)
+    expect(body.message).to.equal('All fields must be filled');
+  })
+
+  afterEach(sinon.restore);
+});
---
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -0,0 +1,28 @@
+const validEmail = 'username@email.com';
+const validPassword = 'password123';
+const hashedPassword = '$2a$10$C52Amv3qMJlqUHdFZCmF3eelLGdYch/1FgO5joYS4W5GN0KGQzSKS';
+
+const bodyWithoutEmail = { email: '', password: validPassword };
+const bodyWithoutPassword = { email: validEmail, password: '' };
+
+const bodyWithInvalidUser = { email: 'invalid_email', password: validPassword };
+const bodyWithInvalidPassword = { email: validEmail, password: 'invalid_password' };
+
+const validBody = { email: validEmail, password: validPassword };
+
+const existingUser = {
+  id: 1,
+  username: 'name',
+  role: 'admin',
+  email: validEmail,
+  password: hashedPassword,
+};
+
+export default {
+  bodyWithoutEmail,
+  bodyWithoutPassword,
+  bodyWithInvalidUser,
+  bodyWithInvalidPassword,
+  validBody,
+  existingUser,
+}
